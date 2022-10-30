172ede49639eea3fe9dcf223e34a34c8aec939b2
coltongraham99 <coltongraham99@gmail.com>
2022-10-30 17:40:22 -0400

    Try 2 to get github to work

---
+++ examples/CMakeFiles/example_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_main.dir/example_main.o"
+  "example_main"
+  "example_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
