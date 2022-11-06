bb3b309cbbd2778dc9f011f708e1f64a4410c696
A-Gray-94 <alachris@umich.edu>
2022-11-06 17:10:53 -0500

    Add unit test of random matrix vector products

--- tests/test_SparseMatrix_JDS.cpp
+++ tests/test_SparseMatrix_JDS.cpp
@@ -21,7 +21,11 @@ JDS Matrix unit tests
 // ==============================================================================
 // Compile-time constants
 // ==============================================================================
-#define N 10
+#define M 40
+#define N 50
+#define RAND_VALUE ((fp_type)rand() / (RAND_MAX))
+#define SPARSITY 0.15
+#define NUM_MATVEC_TESTS 100
 
 // Tests both double and float implementation
 #ifdef TEST_DOUBLE
@@ -39,12 +43,14 @@ using fp_type = float;
 #endif
 #endif
 
-// Check that the matrix has the correct number of rows, columns and non-zeros after setting some entries
+// Check that the matrix has the correct number of rows, columns and non-zeros
+// after setting some entries
 TEST(buildingTest) {
   // Create an NxN JDS matrix
   SpMV::SparseMatrix_JDS<fp_type> matrix(N, N);
 
-  // Check that numRows and numCols are N, that numNonZeros = 0, and that the matrix is in the initialized state
+  // Check that numRows and numCols are N, that numNonZeros = 0, and that the
+  // matrix is in the initialized state
   ASSERT_EQUAL(matrix.getNumRows(), N);
   ASSERT_EQUAL(matrix.getNumCols(), N);
   ASSERT_EQUAL(matrix.getNumNonZeros(), 0);
@@ -63,22 +69,24 @@ TEST(buildingTest) {
   matrix.assembleStorage();
   ASSERT_EQUAL(matrix.getState(), SpMV::assembled);
 
-  // Check that setting another coefficient returns the matrix to the building state
+  // Check that setting another coefficient returns the matrix to the building
+  // state
   matrix.setCoefficient(0, 0, 2.0);
   ASSERT_EQUAL(matrix.getState(), SpMV::building);
 }
 
-// Check that we get the expected behaviour from y = A*x when A is a diagonal matrix
-TEST(IdentityMatVec) {
+// Check that we get the expected behaviour from y = A*x when A is a diagonal
+// matrix
+TEST(DiagonalMatVec) {
   // Create an NxN JDS matrix
   SpMV::SparseMatrix_JDS<fp_type> matrix(N, N);
 
   // Create vectors
   fp_type x[N], y[N], y_expected[N];
 
-  // Set the diagonals to 1 (create an identity matrix)
+  // Set the diagonals to random values
   for (size_t ii = 0; ii < N; ii++) {
-    y_expected[ii] = ((fp_type)rand() / (RAND_MAX));
+    y_expected[ii] = RAND_VALUE;
     matrix.setCoefficient(ii, ii, y_expected[ii]);
   }
 
@@ -86,7 +94,7 @@ TEST(IdentityMatVec) {
 
   // Set x to random values
   for (unsigned int ii = 0; ii < N; ii++) {
-    x[ii] = ((fp_type)rand() / (RAND_MAX));
+    x[ii] = RAND_VALUE;
     y_expected[ii] *= x[ii];
   }
 
@@ -94,6 +102,49 @@ TEST(IdentityMatVec) {
   matrix.computeMatVecProduct(x, y);
   for (unsigned int ii = 0; ii < N; ii++) {
     ASSERT_EQUAL(y[ii], y_expected[ii]);
+  }
+}
+
+// Test a series of random matrix-vector products
+TEST(RandomMatVec) {
+
+  for (int n = 0; n < NUM_MATVEC_TESTS; n++) {
+
+    // Create an MxN JDS matrix
+    SpMV::SparseMatrix_JDS<fp_type> matrix(M, N);
+
+    // Create the vector to be multiplied, the vector to store the MatVec result in, and the vector to store the
+    // expected result in
+    fp_type x[N], y[N], y_expected[N];
+
+    // Set x to random values, expected y to 0
+    for (unsigned int ii = 0; ii < N; ii++) {
+      x[ii] = RAND_VALUE;
+      y_expected[ii] = 0.0;
+    }
+
+    // Now we will go through each entry of the matrix and randomly assign random values
+    for (unsigned int rowInd = 0; rowInd < M; rowInd++) {
+      for (unsigned int colInd = 0; colInd < N; colInd++) {
+        if (RAND_VALUE < SPARSITY) {
+          // Set the coefficient to a random value
+          fp_type value = RAND_VALUE;
+          matrix.setCoefficient(rowInd, colInd, value);
+
+          // Add the value to the expected result
+          y_expected[rowInd] += value * x[colInd];
+        }
+      }
+    }
+
+    // Assemble the matrix
+    matrix.assembleStorage();
+
+    // Do the matrix vector multiplication and check that y contains the row sums of the matrix
+    matrix.computeMatVecProduct(x, y);
+    for (unsigned int ii = 0; ii < N; ii++) {
+      ASSERT_ALMOST_EQUAL(y[ii], y_expected[ii], 1e-10);
+    }
   }
 }
 
