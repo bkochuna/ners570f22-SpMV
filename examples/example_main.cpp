05b85688f4a296e2620f0931555e8b6287ad9b47
camckenn <80509518+camckenn@users.noreply.github.com>
2022-11-07 16:53:04 -0500

    Reporting changes to SparseMatrix_DEN.hpp (#70)
    * Header File for DEN added
    
    * Header File Updated with unAssemble and Destructor for DEN
    
    * example updated
    
    * CMakeLists in src updated
    
    * Update SparseMatrix_DEN.hpp
    
    * Fixed unAssemble and Destructor
    
    * Update SparseMatrix_DEN.hpp
    
    Revise getFormat() after talking with professor about the exact meaning of it.
    
    Co-authored-by: Soumik Das <soumikd@caen-vnc-mi02.engin.umich.edu>
    Co-authored-by: umichyujl <91201750+umichyujl@users.noreply.github.com>
    Co-authored-by: meichenl95 <89050473+meichenl95@users.noreply.github.com>

--- examples/example_main.cpp
+++ examples/example_main.cpp
@@ -1,6 +1,7 @@
 #include "SparseMatrix.hpp"
 #include "SparseMatrix_COO.hpp"
-#include <iostream>
+#include "SparseMatrix_DEN.hpp"
+
 
 template class SpMV::SparseMatrix<float>;  // Forces binary code to be generated for float data type
 template class SpMV::SparseMatrix<double>; // Forces binary code to be generated for double data type
@@ -8,25 +9,34 @@ template class SpMV::SparseMatrix<double>; // Forces binary code to be generated
 int main(int argc, char *argv[]) {
   std::cout << "Hello World!" << std::endl;
 
-  SpMV::SparseMatrix<double> *ptr_A = nullptr;
-  SpMV::SparseMatrix_COO<double> *ptr_B = nullptr;
+
+    SpMV::SparseMatrix<double>* ptr_A = nullptr;
+    SpMV::SparseMatrix_COO<double>* ptr_B = nullptr;
+    SpMV::SparseMatrix_DEN<double>* ptr_C = nullptr;
 
   ptr_A = new SpMV::SparseMatrix_COO<double>(5, 8);
 
   ptr_B = (SpMV::SparseMatrix_COO<double> *)ptr_A;
 
-  ptr_B->setCoefficient(1, 1, 1.0);
-  ptr_B->setCoefficient(2, 2, 2.0);
-  ptr_B->setCoefficient(1, 1, -1.0);
+    ptr_C = (SpMV::SparseMatrix_DEN<double>*)ptr_C;
 
-  ptr_A->assembleStorage();
-  cout << "What does our matrix look like?" << endl;
-  // New scoping unit. This means variables defined in here, stay here.
-  {
-    SpMV::SparseMatrix_COO<float> A = SpMV::SparseMatrix_COO<float>(2, 2);
-    cout << "Do stuff with A" << endl;
-    // A.assembleStorage();
-    // A.setCoefficient(5,6, 1.0);
+    ptr_B->setCoefficient(1,1,1.0);
+    ptr_B->setCoefficient(2,2,2.0);
+    ptr_B->setCoefficient(1,1,-1.0);
+
+    ptr_C->setCoefficient(1,1,1.0);
+    ptr_C->setCoefficient(2,2,2.0);
+
+    ptr_A->assembleStorage();
+    ptr_C->assembleStorage();
+    
+    cout << "What does our matrix look like?" << endl;
+    // New scoping unit. This means variables defined in here, stay here.
+    {
+        SpMV::SparseMatrix_COO<float> A = SpMV::SparseMatrix_COO<float>(2,2); 
+        cout << "Do stuff with A" << endl;
+        //A.assembleStorage();
+        //A.setCoefficient(5,6, 1.0);
 
     // SpMV::SparseMatrix B = A.getFormat();
   }
