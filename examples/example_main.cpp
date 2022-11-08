3ef86009d82bdf8733403f7c3f52973553a7e99d
arshsaja <arshsaja@umich.edu>
2022-11-07 19:09:38 -0500

    doc and example Fixing conflicts

--- examples/example_main.cpp
+++ examples/example_main.cpp
@@ -1,6 +1,5 @@
 #include "SparseMatrix.hpp"
 #include "SparseMatrix_COO.hpp"
-#include "SparseMatrix_CSR.hpp"
 #include "SparseMatrix_DEN.hpp"
 
 template class SpMV::SparseMatrix<float>;  // Forces binary code to be generated
@@ -20,7 +19,6 @@ int main(int argc, char *argv[]) {
   ptr_B = (SpMV::SparseMatrix_COO<double> *)ptr_A;
 
   ptr_C = (SpMV::SparseMatrix_DEN<double> *)ptr_C;
-  ptr_D = (SpMV::SparseMatrix_CSR<double> *)ptr_D;
 
   ptr_B->setCoefficient(1, 1, 1.0);
   ptr_B->setCoefficient(2, 2, 2.0);
@@ -29,12 +27,8 @@ int main(int argc, char *argv[]) {
   ptr_C->setCoefficient(1, 1, 1.0);
   ptr_C->setCoefficient(2, 2, 2.0);
 
-  ptr_D->setCoefficient(1, 1, 1.0);
-  ptr_D->setCoefficient(2, 2, 2.0);
-
   ptr_A->assembleStorage();
   ptr_C->assembleStorage();
-  ptr_D->assembleStorage();
 
   cout << "What does our matrix look like?" << endl;
   // New scoping unit. This means variables defined in here, stay here.
@@ -49,9 +43,6 @@ int main(int argc, char *argv[]) {
 
   delete (ptr_A);
   ptr_A = NULL;
-
-  delete (ptr_D);
-  ptr_D = NULL;
 
   return 0;
 }
