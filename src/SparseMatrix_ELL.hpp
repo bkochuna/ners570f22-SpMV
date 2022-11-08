25300a14eb738048ae25a5c90722d86494e8c28d
Shibo Tan <shibot@umich.edu>
2022-11-07 20:48:49 -0500

    Update SparseMatrix_ELL.hpp
    Update the getformat function

--- src/SparseMatrix_ELL.hpp
+++ src/SparseMatrix_ELL.hpp
@@ -139,19 +139,33 @@ namespace SpMV
     }
 
     template <class fp_type>
-    SparseMartix<fp_type> SparseMatrix_ELL<fp_type>::getFormat()
-    {
-        cout << "Hello from SparseMatrix_ELL::getFormat!" << endl;
-        if(this->_state == assembled): unAssemble();
-        
-        SparseMartix<fp_type> B;
-        
-        B._ncols = this->_ncols;
-        B._nrows = this->_nrows;
-        B._nnz = this->_nnz;
-        B._buildCoeff = this->_buildCoeff;
-        B.assembleStorage()
-        return B;
+    SparseMatrix<fp_type> *SparseMatrix_JDS<fp_type>::getFormat(string fmt) {
+    if (this->_state == assembled) {_unAssemble();}
+    // Create pointer to new matrix that will be returned
+    SparseMatrix<fp_type> *ptr_A = nullptr;
+
+    // Create the new matrix in the requested format for ptr_a to point to ---
+    if (fmt == "DEN") {
+      ptr_A = new SparseMatrix_DEN<fp_type>(this->_nrows, this->_ncols);
+    }
+    else if (fmt == "COO") {
+      ptr_A = new SparseMatrix_COO<fp_type>(this->_nrows, this->_ncols);
+    }
+    else if (fmt == "CSR") {
+      ptr_A = new SparseMatrix_CSR<fp_type>(this->_nrows, this->_ncols);
+    }
+    else if (fmt == "JDS") {
+      ptr_A = new SparseMatrix_JDS<fp_type>(this->_nrows, this->_ncols);
+    }
+    else if (fmt == "ELL") {
+      ptr_A = new SparseMatrix_ELL<fp_type>(this->_nrows, this->_ncols);
+    }
+
+    // Copy the nonzero entry data to the new matrix and assemble it
+    ptr_A->_buildCoeff = this->_buildCoeff;
+    ptr_A->assembleStorage();
+
+    return ptr_A;
     }
 
     template <class fp_type>
