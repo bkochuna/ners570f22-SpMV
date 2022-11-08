ac13de40ee9513976dd317a4652ffa814bbcfdd5
umichyujl <91201750+umichyujl@users.noreply.github.com>
2022-11-07 20:19:39 -0500

    Revise getFormat function
    Fix issues with the abstract class (use new now), following by Alasdair's suggestions.

--- src/SparseMatrix_JDS.hpp
+++ src/SparseMatrix_JDS.hpp
@@ -267,53 +267,32 @@ namespace SpMV {
   SparseMatrix<fp_type>* SparseMatrix_JDS<fp_type>::getFormat(string fmt) {
     if (this->_state == assembled):
       _unAssemble();
-    // variables
-    SparseMatrix<fp_type> B;
-    SparseMatrix<fp-type>* ptr_B = B;
+
+    // Create pointer to new matrix that will be returned
     SparseMatrix<fp-type>* ptr_A = nullptr;
-    // newMat -> _nrows, _ncols, _nnz, _buildCoeff
-    // number of rows
-    B._nrows = this->_nrows;
-    // number of columns
-    B._ncols = this->_ncols;
-    // nonzero values
-    B._nnz = this->_nnz;
-    // _buildCoeff 
-    B._buildCoeff = this->_buildCoeff;
-    // newMat -> assemble
-    B.assembleStorage();
-    // tranform as the format
-    if (fmt == "DEN")
-    {
-      ptr_A = (SparseMatrix_DEN<fp_type> *)ptr_B;
-      ptr_A->assembleStorage();
+
+    // --- Create the new matrix in the requested format for ptr_a to point to ---
+    if (fmt == "DEN") {
+      ptr_A = new SparseMatrix_DEN<fp_type>(this->_nrows, this->_ncols);
     }
-    else if (fmt == "COO")
-    {
-      ptr_A = (SparseMatrix_COO<fp_type> *)ptr_B;
-      ptr_A->assembleStorage();
+    else if (fmt == "COO") {
+      ptr_A = new SparseMatrix_COO<fp_type>(this->_nrows, this->_ncols);
     }
-    else if (fmt == "CSR")
-    {
-      ptr_A = (SparseMatrix_CSR<fp_type> *)ptr_B;
-      ptr_A->assembleStorage();
+    else if (fmt == "CSR") {
+      ptr_A = new SparseMatrix_CSR<fp_type>(this->_nrows, this->_ncols);
     }
-    else if (fmt == "JDS")
-    {
-      ptr_A = (SparseMatrix_JDS<fp_type> *)ptr_B;
-      ptr_A->assembleStorage();
+    else if (fmt == "JDS") {
+      ptr_A = new SparseMatrix_JDS<fp_type>(this->_nrows, this->_ncols);
     }
-    else if (fmt == "ELL")
-    {
-      ptr_A = (SparseMatrix_ELL<fp_type> *)ptr_B;
-      ptr_A->assembleStorage();
+    else if (fmt == "ELL") {
+      ptr_A = new SparseMatrix_ELL<fp_type>(this->_nrows, this->_ncols);
     }
-    else
-    {
-      std::cout << "Wrong Format! Will return a nullptr." << endl;
-    }
+
+    // Copy the nonzero entry data to the new matrix and assemble it
+    ptr_A->_buildCoeff = this->_buildCoeff;
+    ptr_A->assembleStorage();
+
     return ptr_A;
-    //return ptr_B;
   }
 
 } // namespace SpMV
