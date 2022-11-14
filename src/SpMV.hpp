3849a9dbacca8d0078bbef52ec0631247e2106e6
Brendan Kochunas <bkochuna@umich.edu>
2022-11-13 23:01:23 -0500

    Updating SpMV.hpp
    Description:
    Uncommented headers for DEN and COO

--- src/SpMV.hpp
+++ src/SpMV.hpp
@@ -2,8 +2,8 @@
 #define SPMV_H
 
 #include "SparseMatrix.hpp"
-// #include "SparseMatrix_DEN.hpp"
-// #include "SparseMatrix_COO.hpp"
+#include "SparseMatrix_COO.hpp"
+#include "SparseMatrix_DEN.hpp"
 // #include "SparseMatrix_CSR.hpp"
 // #include "SparseMatrix_ELL.hpp"
 #include "SparseMatrix_JDS.hpp"
